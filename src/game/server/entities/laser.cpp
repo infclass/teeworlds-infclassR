1393c316f4b30842aec1fb8b029c361f86631fef
Teoman Reimann <teoman@posteo.de>
2019-03-02 11:24:23 +0100

    fixed medic selfharm

--- src/game/server/entities/laser.cpp
+++ src/game/server/entities/laser.cpp
@@ -53,7 +53,7 @@ bool CLaser::HitCharacter(vec2 From, vec2 To)
 			if (zombie->GetPlayer()->GetCharacter()) {
 				zombie->GetPlayer()->GetCharacter()->SetHealthArmor(1, 0);
 				zombie->Unfreeze();
-				medic->TakeDamage(vec2(0.f, 0.f), DAMAGE_ON_REVIVE * 2, m_Owner, WEAPON_RIFLE, TAKEDAMAGEMODE_NOINFECTION);
+				medic->TakeDamage(vec2(0.f, 0.f), DAMAGE_ON_REVIVE * 2, m_Owner, WEAPON_RIFLE, TAKEDAMAGEMODE_SELFHARM);
 				str_format(aBuf, sizeof(aBuf), "Medic %s revived %s",
 						Server()->ClientName(medic->GetPlayer()->GetCID()),
 						Server()->ClientName(zombie->GetPlayer()->GetCID()));
