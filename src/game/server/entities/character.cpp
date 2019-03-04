fcd07c686a1560d614779d4fad732d70f19a2e5a
breton <breton@localhost>
2019-03-04 19:35:56 +0100

    Revert "fixed indentation"
    This reverts commit 3b1055de9b76f5eec0388a5cacde2bf921620fd2.

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -553,8 +553,7 @@ void CCharacter::UpdateTuningParam()
 	{
 		pTuningParams->m_Gravity = 0.0f;
 	}
-	
-	pTuningParams->m_PlayerHooking = 0;
+		pTuningParams->m_PlayerHooking = 0;
 	
 	if(GetClass() == PLAYERCLASS_GHOUL)
 	{
