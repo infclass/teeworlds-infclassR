944ad4a9f6d61041232de362bbcd7b1dc8b632dc
Alexander Akulich <akulichalexander@gmail.com>
2019-11-07 20:50:05 +0300

    CPlayer: Remove unused method FakeSnap()

--- src/game/server/player.h
+++ src/game/server/player.h
@@ -28,7 +28,6 @@ public:
 	void Tick();
 	void PostTick();
 	void Snap(int SnappingClient);
-	void FakeSnap(int SnappingClient);
 
 	void OnDirectInput(CNetObj_PlayerInput *NewInput);
 	void OnPredictedInput(CNetObj_PlayerInput *NewInput);
