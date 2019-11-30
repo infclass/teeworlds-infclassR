a75c6e1da16d058c60aaa75747d793a9ba4e1da8
Alexander Akulich <akulichalexander@gmail.com>
2019-11-30 22:11:03 +0300

    Character: Check for Player on Destroy called
    We have such a check in other places, so add the check here just for a case.

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -236,7 +236,8 @@ void CCharacter::Destroy()
 /* INFECTION MODIFICATION START ***************************************/
 	DestroyChildEntities();
 	
-	m_pPlayer->ResetNumberKills();
+	if (m_pPlayer)
+		m_pPlayer->ResetNumberKills();
 	
 	if(m_FlagID >= 0)
 	{
@@ -272,7 +273,8 @@ void CCharacter::Destroy()
 	
 /* INFECTION MODIFICATION END *****************************************/
 
-	GameServer()->m_World.m_Core.m_apCharacters[m_pPlayer->GetCID()] = 0;
+	if (m_pPlayer)
+		GameServer()->m_World.m_Core.m_apCharacters[m_pPlayer->GetCID()] = 0;
 	m_Alive = false;
 }
 
