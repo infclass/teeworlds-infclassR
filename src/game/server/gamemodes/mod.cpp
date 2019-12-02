5d114035db786f532c38aa174d0e2b5fcf074d5e
Alexander Akulich <akulichalexander@gmail.com>
2019-12-02 21:45:54 +0300

    Disable zombie spawning at witch with open portals

--- src/game/server/gamemodes/mod.cpp
+++ src/game/server/gamemodes/mod.cpp
@@ -1006,6 +1006,7 @@ bool CGameControllerMOD::PreSpawn(CPlayer* pPlayer, vec2 *pOutPos)
 			if(Iter.Player()->GetCID() == pPlayer->GetCID()) continue;
 			if(Iter.Player()->GetClass() != PLAYERCLASS_WITCH) continue;
 			if(!Iter.Player()->GetCharacter()) continue;
+			if (Iter.Player()->GetCharacter()->HasPortal()) continue;
 			
 			vec2 SpawnPos;
 			if(Iter.Player()->GetCharacter()->FindWitchSpawnPosition(SpawnPos))
