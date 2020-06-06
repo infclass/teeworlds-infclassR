8a24df6ae885d4bac49634e2a7d744b1233eab1e
BloodWod <33897884+BloodWod@users.noreply.github.com>
2020-06-07 03:02:56 +0500

    Separate file for changelog

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -4538,35 +4538,3 @@ void CGameContext::Converse(int ClientID, const char* pStr, int team)
 		PrivateMessage(aBuf, ClientID, (team != CGameContext::CHAT_ALL));
 	}
 }
-
-bool CGameContext::ConChangeLog(IConsole::IResult *pResult, void *pUserData)
-{
-	CGameContext *pSelf = (CGameContext *)pUserData;
-
-	int ClientID = pResult->GetClientID();
-
-	// if we don't get the parameter, the list = 1
-	int list = pResult->GetInteger(0); 
-	if (!list)
-		list = 1; 
-	int lists = 1;
-	char aBuf[256];
-	str_format(aBuf, sizeof(aBuf), "list %d/%d", list, lists);
-
-	switch(list)
-	{
-		case 1:
-			pSelf->SendChatTarget(ClientID, "----- Changelog -----");
-			pSelf->SendChatTarget(ClientID, "Fix Self killing/spectator gives you points.");
-			pSelf->SendChatTarget(ClientID, "Add new weapon rifle for mercenary's bomb.");
-			pSelf->SendChatTarget(ClientID, "Add converse (/c \"text\") for whisper-chat.");
-			pSelf->SendChatTarget(ClientID, "Add funround ghosts vs ninjas.");
-			pSelf->SendChatTarget(ClientID, "Add funround ghouls vs biologists.");
-			pSelf->SendChatTarget(ClientID, "-------------------------");
-			break;	
-		default:
-			pSelf->SendChatTarget(ClientID, "this list does not exist.");
-			break;
-	}
-	return true;
-}
