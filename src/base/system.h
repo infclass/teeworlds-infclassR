46695fa029d90cbb790a01823ece1e295fc148e7
Alexander Akulich <akulichalexander@gmail.com>
2021-02-09 21:56:49 +0300

    base/system: Add a type for uint64

--- src/base/system.h
+++ src/base/system.h
@@ -446,9 +446,12 @@ void lock_unlock(LOCK lock);
 	not being a C90 thing.
 */
 __extension__ typedef long long int64;
+__extension__ typedef unsigned long long uint64;
 #else
 typedef long long int64;
+typedef unsigned long long uint64;
 #endif
+
 /*
 	Function: time_get
 		Fetches a sample from a high resolution timer.
