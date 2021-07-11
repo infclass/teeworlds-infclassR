cf8e60c870d10f341e4db48a2e95f78b3d86c02a
Alexander Akulich <akulichalexander@gmail.com>
2021-07-11 16:53:32 +0300

    CInfClassPlayerClass: Introduce OnCharacterDeath()

--- src/game/server/infclass/classes/infcplayerclass.cpp
+++ src/game/server/infclass/classes/infcplayerclass.cpp
@@ -204,6 +204,10 @@ void CInfClassPlayerClass::OnCharacterSpawned()
 	GiveClassAttributes();
 }
 
+void CInfClassPlayerClass::OnCharacterDeath(int Weapon)
+{
+}
+
 void CInfClassPlayerClass::OnWeaponFired(WeaponFireContext *pFireContext)
 {
 	switch(pFireContext->Weapon)
