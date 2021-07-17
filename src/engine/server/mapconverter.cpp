f2105838874a10c4465ee961a82e5163df61b3a3
Alexander Akulich <akulichalexander@gmail.com>
2021-07-18 02:15:48 +0300

    MapConverter: Implement Infclass game tiles conversion to DDNet tiles

--- src/engine/server/mapconverter.cpp
+++ src/engine/server/mapconverter.cpp
@@ -1,6 +1,7 @@
 #include <engine/server/mapconverter.h>
 
 #include <base/color.h>
+#include <game/layers.h>
 #include <game/server/classes.h>
 #include <game/server/infclass/classes/humans/human.h>
 #include <game/server/infclass/infcgamecontroller.h>
@@ -8,13 +9,80 @@
 
 #include <pnglite.h>
 
-int GetClientGameTileIndex(int PhysicalIndex)
+enum DDNET_EXTRA_GAME_TILES
+{
+	TILE_AIR = 0,
+	TILE_SOLID,
+	TILE_DEATH,
+	TILE_NOHOOK,
+	TILE_NOLASER,
+	TILE_THROUGH_CUT,
+	TILE_THROUGH,
+	TILE_JUMP,
+	TILE_FREEZE = 9,
+	TILE_TELEINEVIL,
+	TILE_UNFREEZE,
+	TILE_DFREEZE,
+	TILE_DUNFREEZE,
+	TILE_TELEINWEAPON,
+	TILE_TELEINHOOK,
+	TILE_WALLJUMP = 16,
+	TILE_EHOOK_ENABLE,
+	TILE_EHOOK_DISABLE,
+	TILE_HIT_ENABLE,
+	TILE_HIT_DISABLE,
+	TILE_SOLO_ENABLE,
+	TILE_SOLO_DISABLE,
+	//Switches
+	TILE_SWITCHTIMEDOPEN = 22,
+	TILE_SWITCHTIMEDCLOSE,
+	TILE_SWITCHOPEN,
+	TILE_SWITCHCLOSE,
+	TILE_TELEIN,
+	TILE_TELEOUT,
+	TILE_BOOST,
+	TILE_TELECHECK,
+	TILE_TELECHECKOUT,
+	TILE_TELECHECKIN,
+	TILE_REFILL_JUMPS = 32,
+	TILE_START,
+	TILE_FINISH,
+	TILE_CHECKPOINT_FIRST = 35,
+	TILE_CHECKPOINT_LAST = 59,
+	TILE_STOP = 60,
+	TILE_STOPS,
+	TILE_STOPA,
+};
+
+int GetClientGameTileIndex(int PhysicalIndex, int icDamageIndex, int icBonusIndex)
 {
 	switch(PhysicalIndex)
 	{
 		case TILE_PHYSICS_SOLID:
 		case TILE_PHYSICS_NOHOOK:
 			return PhysicalIndex;
+		default:
+			break;
+	}
+
+	switch(icDamageIndex)
+	{
+		case ZONE_DAMAGE_DEATH:
+			return TILE_DEATH;
+		case ZONE_DAMAGE_DEATH_NOUNDEAD:
+			return TILE_HIT_DISABLE;
+		case ZONE_DAMAGE_DEATH_INFECTED:
+			return TILE_DUNFREEZE;
+		case ZONE_DAMAGE_INFECTION:
+			return TILE_TELEINWEAPON;
+		default:
+			break;
+	}
+
+	switch(icBonusIndex)
+	{
+		case ZONE_BONUS_BONUS:
+			return TILE_THROUGH_CUT;
 		default:
 			break;
 	}
@@ -615,15 +683,29 @@ void CMapConverter::CopyGameLayer()
 	Item.m_Flags = 1;
 	Item.m_Image = -1;
 	
+	CLayers Layers;
+	Layers.Init(Map());
+
+	CCollision Collision;
+	Collision.Init(&Layers);
+
+	int ZoneHandle_icDamage = Collision.GetZoneHandle("icDamage");
+	int ZoneHandle_icBonus = Collision.GetZoneHandle("icBonus");
+
 	//Cleanup the game layer
 	//This will make maps no more usable by a server, but the original ones are in the repository
 	for(int j=0; j<m_Height; j++)
 	{
 		for(int i=0; i<m_Width; i++)
 		{
-			int PhysicalIndex = m_pPhysicsLayerTiles[j*m_Width+i].m_Index;
+			float X = i * 32 + 16;
+			float Y = j * 32 + 16;
 
-			m_pTiles[j*m_Width+i].m_Index = GetClientGameTileIndex(PhysicalIndex);
+			int PhysicalIndex = m_pPhysicsLayerTiles[j*m_Width+i].m_Index;
+			int icDamageIndex = Collision.GetZoneValueAt(ZoneHandle_icDamage, X, Y);
+			int icBonusIndex = Collision.GetZoneValueAt(ZoneHandle_icBonus, X, Y);
+
+			m_pTiles[j*m_Width+i].m_Index = GetClientGameTileIndex(PhysicalIndex, icDamageIndex, icBonusIndex);
 
 			i += m_pPhysicsLayerTiles[j*m_Width+i].m_Skip;
 		}
