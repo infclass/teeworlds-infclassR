ea87104f85f0f48708f9810f04136fdf18c39c80
Alexander Akulich <akulichalexander@gmail.com>
2021-08-15 02:03:59 +0300

    CGameContext: De-virtualize SendChatTarget()

--- src/game/server/gamecontext.h
+++ src/game/server/gamecontext.h
@@ -204,7 +204,7 @@ public:
 
 	// network
 	void CallVote(int ClientID, const char *aDesc, const char *aCmd, const char *pReason, const char *aChatmsg);
-	virtual void SendChatTarget(int To, const char *pText);
+	void SendChatTarget(int To, const char *pText);
 	void SendChat(int ClientID, int Team, const char *pText);
 	void SendEmoticon(int ClientID, int Emoticon);
 	void SendWeaponPickup(int ClientID, int Weapon);
