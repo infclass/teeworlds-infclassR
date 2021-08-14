TeeworldsCN/ddnet-pvp

Allow utf8 ban reasons (fixes #3329)

src/engine/shared/network_conn.cpp
@@ -294,11 +294,8 @@ int CNetConnection::Feed(CNetPacketConstruct *pPacket, NETADDR *pAddr, SECURITY_
 				if(pPacket->m_DataSize > 1)
 				{
 					// make sure to sanitize the error string form the other party
-					if(pPacket->m_DataSize < 128)
-						str_copy(aStr, (char *)&pPacket->m_aChunkData[1], pPacket->m_DataSize);
-					else
-						str_copy(aStr, (char *)&pPacket->m_aChunkData[1], sizeof(aStr));
-					str_sanitize_strong(aStr);
+					str_utf8_copy(aStr, (char *)&pPacket->m_aChunkData[1], minimum(pPacket->m_DataSize, (int)sizeof(aStr)));
+					str_sanitize_cc(aStr);
 				}
 
 				if(!m_BlockCloseMsg)

infclass/teeworlds-infclassR

(cherry picked from commit d99f1b934022f4483c1dec1ab74dfb1e4f4621ad)

src/engine/shared/network_conn.cpp
@@ -285,11 +285,8 @@ int CNetConnection::Feed(CNetPacketConstruct *pPacket, NETADDR *pAddr)
 				if(pPacket->m_DataSize > 1)
 				{
 					// make sure to sanitize the error string form the other party
-					if(pPacket->m_DataSize < 128)
-						str_copy(Str, (char *)&pPacket->m_aChunkData[1], pPacket->m_DataSize);
-					else
-						str_copy(Str, (char *)&pPacket->m_aChunkData[1], sizeof(Str));
-					str_sanitize_strong(Str);
+					str_utf8_copy(Str, (char *)&pPacket->m_aChunkData[1], minimum(pPacket->m_DataSize, (int)sizeof(Str)));
+					str_sanitize_cc(Str);
 				}
 
 				if(!m_BlockCloseMsg)