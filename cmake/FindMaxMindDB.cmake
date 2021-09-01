7b3ca8732c5997e4f02ee7c2cafca8fe704f816f
Alexander Akulich <akulichalexander@gmail.com>
2021-09-01 23:07:35 +0300

    CMake/FindMaxMindDB: Implement the QUIET option

--- cmake/FindMaxMindDB.cmake
+++ cmake/FindMaxMindDB.cmake
@@ -1,27 +1,33 @@
 include(FindPackageHandleStandardArgs)
 include(CMakeFindDependencyMacro)
 
-# Use system OGG
 find_dependency(PkgConfig)
-pkg_check_modules(PC_MaxMindDB REQUIRED libmaxminddb)
+pkg_check_modules(PC_MaxMindDB QUIET libmaxminddb)
 
-if(PC_MaxMindDB_FOUND)
+find_package_handle_standard_args(MaxMindDB
+  REQUIRED_VARS
+    PC_MaxMindDB_LIBRARIES
+  VERSION_VAR
+    PC_MaxMindDB_VERSION
+)
+
+if(MaxMindDB_FOUND)
   set(MaxMindDB_LIBRARIES ${PC_MaxMindDB_LIBRARIES})
-  set(MaxMindDB_INCLUDE_DIRS ${PC_MaxMindDB_INCLUDE_DIRS})
-
-  mark_as_advanced(
-      MaxMindDB_LIBRARIES
-      MaxMindDB_INCLUDE_DIRS
-  )
-
-  set(MaxMindDB_FOUND TRUE)
-  add_library(MaxMindDB::MaxMindDB INTERFACE IMPORTED)
-  if(MaxMindDB_INCLUDE_DIRS)
-      set_property(TARGET MaxMindDB::MaxMindDB PROPERTY
-          INTERFACE_INCLUDE_DIRECTORIES "${MaxMindDB_INCLUDE_DIRS}"
-      )
+  if(PC_MaxMindDB_INCLUDE_DIRS)
+    set(MaxMindDB_INCLUDE_DIRS ${PC_MaxMindDB_INCLUDE_DIRS})
+  else()
+    unset(MaxMindDB_INCLUDE_DIRS)
   endif()
-  set_property(TARGET MaxMindDB::MaxMindDB PROPERTY
-      INTERFACE_LINK_LIBRARIES "${MaxMindDB_LIBRARIES}"
-  )
+
+  if(NOT TARGET MaxMindDB::MaxMindDB)
+    add_library(MaxMindDB::MaxMindDB INTERFACE IMPORTED)
+    if(MaxMindDB_INCLUDE_DIRS)
+        set_property(TARGET MaxMindDB::MaxMindDB PROPERTY
+            INTERFACE_INCLUDE_DIRECTORIES "${MaxMindDB_INCLUDE_DIRS}"
+        )
+    endif()
+    set_property(TARGET MaxMindDB::MaxMindDB PROPERTY
+        INTERFACE_LINK_LIBRARIES "${MaxMindDB_LIBRARIES}"
+    )
+  endif()
 endif()
