b8cefa4958f437f6c2ed5d8534dc9376a27ed170
Alexander Akulich <akulichalexander@gmail.com>
2021-11-30 00:47:08 +0300

    CMercenaryLaser now collides only with the owner bomb

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ## InfclassR v1.3.1 - 2021-xx-xx (unreleased)
 
+- Merc laser now collides only with the owner bomb
 - Fixed crash on a new round if a Voodoo finished the previous round on a death tile
 
 ## InfclassR v1.3.0 - 2021-11-27
--- src/game/server/infclass/entities/merc-laser.cpp
+++ src/game/server/infclass/entities/merc-laser.cpp
@@ -11,6 +11,20 @@
 
 static const int MercLaserDamage = 0;
 
+static int FilterOwnerID = -1;
+
+static bool OwnerFilter(const CEntity *pEntity)
+{
+	const CInfCEntity *pInfEntity = static_cast<const CInfCEntity *>(pEntity);
+	return pInfEntity->GetOwner() == FilterOwnerID;
+}
+
+static CGameWorld::EntityFilter GetOwnerFilter(int Owner)
+{
+	FilterOwnerID = Owner;
+	return OwnerFilter;
+}
+
 CMercenaryLaser::CMercenaryLaser(CGameContext *pGameContext, vec2 Pos, vec2 Direction, float StartEnergy, int Owner)
 	: CInfClassLaser(pGameContext, Pos, Direction, StartEnergy, Owner, MercLaserDamage, CGameWorld::ENTTYPE_LASER)
 {
@@ -21,15 +35,12 @@ CMercenaryLaser::CMercenaryLaser(CGameContext *pGameContext, vec2 Pos, vec2 Dire
 bool CMercenaryLaser::HitCharacter(vec2 From, vec2 To)
 {
 	vec2 At;
-	CEntity *pHitMercBomb = GameWorld()->IntersectEntity(m_Pos, To, 80.0f, &At, CGameWorld::ENTTYPE_MERCENARY_BOMB);
+	CEntity *pHitMercBomb = GameWorld()->IntersectEntity(m_Pos, To, 80.0f, &At, CGameWorld::ENTTYPE_MERCENARY_BOMB, GetOwnerFilter(GetOwner()));
 	if(pHitMercBomb)
 	{
 		CMercenaryBomb *pBomb = static_cast<CMercenaryBomb*>(pHitMercBomb);
-		if(pBomb->GetOwner() == GetOwner())
-		{
-			pBomb->IncreaseDamage(WEAPON_LASER);
-			GameServer()->CreateSound(GetPos(), SOUND_PICKUP_ARMOR);
-		}
+		pBomb->IncreaseDamage(WEAPON_LASER);
+		GameServer()->CreateSound(GetPos(), SOUND_PICKUP_ARMOR);
 
 		m_From = From;
 		m_Pos = At;
