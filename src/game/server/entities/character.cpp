e21ddb7afef707fa7d27b5c7a9f8a60986799a04
Alexander Akulich <akulichalexander@gmail.com>
2022-02-17 21:21:56 +0300

    character.cpp: Cleanup the headers

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -1,13 +1,10 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.				*/
 
-#include <base/math.h>
-#include <base/vmath.h>
-#include <new>
 #include <engine/shared/config.h>
-#include <engine/server/roundstatistics.h>
 #include <game/server/gamecontext.h>
 #include <game/server/player.h>
+#include <new>
 #include <iostream>
 
 #include "character.h"
