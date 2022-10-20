f0be2df56ba7e6bebe15705f0b817abde8c90814
Alexander Akulich <akulichalexander@gmail.com>
2022-10-20 23:36:12 +0300

    Collision: Add default nullptr args

--- src/game/collision.h
+++ src/game/collision.h
@@ -59,7 +59,7 @@ public:
 	int GetCollisionAt(float x, float y) const { return GetTile(round(x), round(y)); }
 	int GetWidth() const { return m_Width; };
 	int GetHeight() const { return m_Height; };
-	int IntersectLine(vec2 Pos0, vec2 Pos1, vec2 *pOutCollision, vec2 *pOutBeforeCollision) const;
+	int IntersectLine(vec2 Pos0, vec2 Pos1, vec2 *pOutCollision = nullptr, vec2 *pOutBeforeCollision = nullptr) const;
 	void MovePoint(vec2 *pInoutPos, vec2 *pInoutVel, float Elasticity, int *pBounces) const;
 	void MoveBox(vec2 *pInoutPos, vec2 *pInoutVel, vec2 Size, float Elasticity) const;
 	bool TestBox(vec2 Pos, vec2 Size) const;
