5229a77afceff11f11cd331805001e5ce45564e0
Alexander Akulich <akulichalexander@gmail.com>
2023-02-19 01:21:34 +0300

    Tests: Add a test for icArray

---
+++ src/tests/test_icArray.cpp
@@ -0,0 +1,48 @@
+#include <gtest/gtest.h>
+
+#include <base/tl/ic_array.h>
+
+TEST(ICArrayReverse, BaseTest)
+{
+	icArray<int, 10> Array1;
+	EXPECT_EQ(Array1.Size(), 0);
+	Array1.Add(0);
+	Array1.Add(1);
+	Array1.Add(2);
+	Array1.Add(3);
+	EXPECT_EQ(Array1.Size(), 4);
+
+	Array1.RemoveAt(2);
+	EXPECT_EQ(Array1.Size(), 3);
+
+	EXPECT_EQ(Array1.At(0), 0);
+	EXPECT_EQ(Array1.At(1), 1);
+	EXPECT_EQ(Array1.At(2), 3);
+
+	int Index = 0;
+	for(int Value : Array1)
+	{
+		EXPECT_EQ(Value, Array1.At(Index));
+		Index++;
+	}
+
+	const auto Array2(Array1);
+	EXPECT_EQ(Array1.Size(), Array2.Size());
+
+	Index = 0;
+	for(int Value : Array2)
+	{
+		EXPECT_EQ(Value, Array1.At(Index));
+		EXPECT_EQ(Value, Array2.At(Index));
+		Index++;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	::testing::InitGoogleTest(&argc, const_cast<char **>(argv));
+
+	int Result = RUN_ALL_TESTS();
+
+	return Result;
+}
