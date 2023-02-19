infclass/teeworlds-infclassR

commit f6218bf53a4992e1fc8629036c532cf6528b37eb

CInfClassCharacter::OnCharacterInInfectionZone: Drop SpawnProtection
The protection is still actually given on the infected spawned (but
not triggered anymore on inf entered the inf zone).

src/game/server/infclass/entities/infccharacter.cpp
@@ -107,10 +107,6 @@ void CInfClassCharacter::OnCharacterInInfectionZone()
 				IncreaseHealth(1);
 			}
 		}
-		if (m_InfZoneTick < 0) {
-			m_InfZoneTick = Server()->Tick(); // Save Tick when zombie enters infection zone
-			GrantSpawnProtection();
-		}
 	}
 	else
 	{

commit 3ab39e1eca2887c43deb970fbec9e74811bafe34

Fix spawn protection (broken after f6218bf53a4992e1fc8629036c532cf6528)

src/game/server/entities/character.cpp
@@ -36,7 +36,7 @@ CCharacter::CCharacter(CGameWorld *pWorld, IConsole *pConsole) :
 	m_IsInSlowMotion = false;
 	m_FrozenTime = -1;
 	m_DartLifeSpan = -1;
-	m_InfZoneTick = -1;
+	m_InfZoneTick = 0;
 	m_InAirTick = 0;
 	m_InWater = 0;
 	m_ProtectionTick = 0;

src/game/server/infclass/entities/infccharacter.cpp
@@ -97,6 +97,10 @@ void CInfClassCharacter::OnCharacterInInfectionZone()
 			{
 				IncreaseHealth(1);
 			}
+			if(m_InfZoneTick < 0)
+			{
+				m_InfZoneTick = Server()->Tick(); // Save Tick when zombie enters infection zone
+			}
 		}
 	}
 	else
@@ -122,7 +126,18 @@ void CInfClassCharacter::OnCharacterInInfectionZone()
 
 void CInfClassCharacter::OnCharacterOutOfInfectionZone()
 {
-	m_InfZoneTick = -1;// Reset Tick when zombie is not in infection zone
+	if(m_InfZoneTick == -1)
+		return;
+
+	m_InfZoneTick = -1; // Reset Tick when zombie is not in infection zone
+
+	if(!m_IsInvisible)
+	{
+		SetEmote(EMOTE_NORMAL, Server()->Tick() + Server()->TickSpeed());
+	}
+
+	// Player left spawn before protection ran out
+	m_ProtectionTick = 0;
 }
 
 void CInfClassCharacter::OnCharacterInBonusZoneTick()
@@ -2122,16 +2137,6 @@ void CInfClassCharacter::PreCoreTick()
 	if(m_ProtectionTick > 0)
 	{
 		--m_ProtectionTick;
-
-		// Player left spawn before protection ran out
-		if(m_InfZoneTick == -1)
-		{
-			if(!m_IsInvisible)
-			{
-				SetEmote(EMOTE_NORMAL, Server()->Tick() + Server()->TickSpeed());
-			}
-			m_ProtectionTick = 0;
-		}
 	}
 
 	// Ghost