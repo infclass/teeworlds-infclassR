38c5ff303abc5ec62fda2ab024262339425cfcb5
Alexander Akulich <akulichalexander@gmail.com>
2024-07-09 23:59:10 +0300

    Update the sources URL

--- src/game/server/infclass/infc_config_variables.h
+++ src/game/server/infclass/infc_config_variables.h
@@ -11,7 +11,7 @@
 
 MACRO_CONFIG_STR(PlayerLogfile, playerlogfile, 128, "", CFGFLAG_SERVER, "Filename to log joined players info to")
 
-MACRO_CONFIG_STR(AboutSourceUrl, about_source_url, 128, "https://github.com/InfectionDust/teeworlds-infclassR", CFGFLAG_SERVER, "The server source code URL")
+MACRO_CONFIG_STR(AboutSourceUrl, about_source_url, 128, "https://github.com/infclass/teeworlds-infclassR", CFGFLAG_SERVER, "The server source code URL")
 MACRO_CONFIG_STR(AboutTranslationUrl, about_translation_url, 128, "https://crowdin.com/translate/infectiondust", CFGFLAG_SERVER, "The translation site URL")
 MACRO_CONFIG_STR(AboutContactsDiscord, about_contacts_discord, 128, "https://infclass.github.io/discord", CFGFLAG_SERVER, "Discord server invite URL")
 MACRO_CONFIG_STR(AboutContactsTelegram, about_contacts_telegram, 128, "", CFGFLAG_SERVER, "Telegram URL or ID")
